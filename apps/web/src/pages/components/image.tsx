6410a60e8fa7860b07bc62ff6415dfc698dcaa9e
zila <rimsila.itc@gmail.com>
2023-05-24 13:21:04 +0700

    update blur img

--- apps/web/src/pages/components/image.tsx
+++ apps/web/src/pages/components/image.tsx
@@ -16,6 +16,9 @@ export type ImageProps = Pick<
   blurImg?: string;
 };
 
+const BLUR_IMG =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAYAAAD68A/GAAAA70lEQVR4AQXBTUvCcADA4Z/7z9xcmbEpdSg6CBYGRUcr6WJvUESfqHN07Jt47hBdBV/AFg2FYTGd2DZzmJiz54ndnpUX51cldvcvSWoqufwGcVlgmlX01CpLmsbD4xMiZ2zeS8MQNRgQX1Gweg6/owGvLxUyxjq2+4VtO8haPIGKxKfdxfpzmUgKruMSxX4QiqDfs2i1A+RwMmM+hsr4nbSvcnNaZHnqE8yTNDpvROGU7FYSueX3MUcuqbUExYsd9jICY66yyOrUm22aHY/vmYec304jRIRuaJROjmg8V/G8IXflawoHhxx/1KjVu/wDycJbrJ7yuz0AAAAASUVORK5CYII=";
+
 function ImageComp({
   disableCache = false,
   src,
@@ -25,7 +28,7 @@ function ImageComp({
   loadingElement,
   blurredProps,
   imgProps,
-  blurImg = "./blurry.svg",
+  blurImg = BLUR_IMG,
 }: ImageProps) {
   const [imageLoaded, setImageLoaded] = useState(false);
 
--- apps/web/src/pages/components/index.page.tsx
+++ apps/web/src/pages/components/index.page.tsx
@@ -6,7 +6,7 @@ const ImageCop = dynamic(() => import("./image"), { ssr: false });
 
 export default function components() {
   const imgUrl =
-    "https://images.unsplash.com/photo-1614849427248-287c4e88ef58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=5000&q=100";
+    "https://images.unsplash.com/photo-1614849427248-287c4e88ef58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&q=80";
   return (
     <div className="min-h-[93vh] bg-gray-100 p-6">
       <div className="flex flex-col gap-y-4">
@@ -15,7 +15,7 @@ export default function components() {
           <ImageCop src={imgUrl} width="100%" />
         </div>
         <div>
-          <h3>Components Image</h3>
+          <h3>Normal Image</h3>
           <img src={imgUrl} width="100%" loading="lazy" />
         </div>
       </div>
